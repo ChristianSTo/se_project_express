d7ff762d924afca8b647b6e7f1b0e5244f0c3a25
ChristianTo <christian.to@outlook.com>
2024-12-05 03:36:25 -0600

    git test

--- utils/config.js
+++ utils/config.js
@@ -2,4 +2,4 @@ const { JWT_SECRET = "super-strong-secret" } = process.env;
 
 module.exports = { JWT_SECRET };
 
-//fdcb4247788f13af5423a5fbb92164342e7353cc2716b3f6b746d6780014bd49
+// fdcb4247788f13af5423a5fbb92164342e7353cc2716b3f6b746d6780014bd49
